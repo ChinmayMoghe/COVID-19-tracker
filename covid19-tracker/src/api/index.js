02d83229e07c8bcaec808a863d060759e6ddb074
chinmay <chinmay11.moghe@gmail.com>
2020-04-18 20:58:15 +0530

    Added Charts with daily data representation of infected and deaths

--- covid19-tracker/src/api/index.js
+++ covid19-tracker/src/api/index.js
@@ -2,6 +2,24 @@ import axios from 'axios';
 const url =  'https://covid19.mathdro.id/api';
 
 export const fetchData = async ()=>{
-    const {data:{confirmed,recovered,deaths,lastUpdate}} = await axios.get(url);
-    return {confirmed,recovered,deaths,lastUpdate};
+    try {
+        const {data:{confirmed,recovered,deaths,lastUpdate}} = await axios.get(url);
+        return {confirmed,recovered,deaths,lastUpdate};
+    } catch (error) {
+        //in case of errors
+    }
+};
+
+export const fetchDailyData = async ()=>{
+    try {
+        const {data} = await axios.get(`${url}/daily`);
+        const modifiedData = data.map((dailyData)=>({
+            confirmed:dailyData.confirmed.total,
+            deaths:dailyData.deaths.total,
+            date:dailyData.reportDate
+        }));
+        return modifiedData;
+    } catch (error) {
+        
+    }
 };
--- covid19-tracker/src/components/Charts/Charts.jsx
+++ covid19-tracker/src/components/Charts/Charts.jsx
@@ -1,8 +1,47 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import {fetchDailyData} from '../../api';
+import styles from './Charts.module.css';
+import {Line,Bar} from 'react-chartjs-2';
+
 
 const Charts = ()=>{
+    /*useState is used for maintaining state inside functional components*/
+    /*Array destructuring is used so as to assign state and setState function*/
+    const [dailyData,setDailyData] = useState([]);
+
+    useEffect(()=>{
+        const fetchedDailyData = async ()=>{
+            setDailyData(await fetchDailyData());
+        };
+        fetchedDailyData();
+    });
+    const lineChart =(
+        dailyData.length?
+        (<Line
+        data = {{
+            labels:dailyData.map(({date})=>date),
+            datasets:[
+            {
+                data:dailyData.map(({confirmed})=>confirmed),
+                label:'Infected',
+                borderColor:'#3333ff',
+                fill:true
+            },{
+                data:dailyData.map(({deaths})=>deaths),
+                label:'Deaths',
+                borderColor:'rgba(255,0,0)',
+                backgroundColor:'rgba(255,0,0,0.5)',
+                fill:true
+            }]
+        }}
+        >
+
+        </Line>):null
+    );
     return (
-        <h2>Charts</h2>
+        <div className={styles.container}>
+            {lineChart}
+        </div>
     )
 };
 
