771e8ac24b10dfd8e222809b0535a15b8bb26c03
chinmay <chinmay11.moghe@gmail.com>
2020-04-18 20:21:17 +0530

    Added Cards to the app

--- covid19-tracker/src/App.js
+++ covid19-tracker/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import {Cards,Charts,CountryPicker} from './components';
-import styles from './App.module.css'
+import styles from './App.module.css';
+import { fetchData } from './api';
 class App extends React.Component {
+  state = {
+    data:{}
+  }
+ async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({data:fetchedData});
+  }
   render() {
+    const {data} = this.state;
     return (
       <div className ={styles.container}>
-        <h1>Covid19-Tracker</h1>
-        <Cards/>
+        <Cards data={data}/>
         <CountryPicker/>
         <Charts/>
       </div>
