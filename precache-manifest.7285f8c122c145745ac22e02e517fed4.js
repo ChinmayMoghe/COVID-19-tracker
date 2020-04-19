2da5217458867da2de15597dcd17906b2747ef56
chinmay <chinmay11.moghe@gmail.com>
2020-04-20 01:11:36 +0530

    Updates

---
+++ precache-manifest.7285f8c122c145745ac22e02e517fed4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e7296462a806010bc76e99518b2f31",
+    "url": "/COVID-19-tracker/index.html"
+  },
+  {
+    "revision": "1922429fcfd7825fb4d4",
+    "url": "/COVID-19-tracker/static/css/main.6a9ddf6f.chunk.css"
+  },
+  {
+    "revision": "d9c36aa10d817f2a43fe",
+    "url": "/COVID-19-tracker/static/js/2.b932e49e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-tracker/static/js/2.b932e49e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1922429fcfd7825fb4d4",
+    "url": "/COVID-19-tracker/static/js/main.946bd7d7.chunk.js"
+  },
+  {
+    "revision": "0b067ae88a27cc616679",
+    "url": "/COVID-19-tracker/static/js/runtime-main.58659696.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-tracker/static/media/coronavirus.d7265326.png"
+  }
+]);
