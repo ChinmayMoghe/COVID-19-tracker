74326b8533683d640a9ddccb8f12f38832b2e754
chinmay <chinmay11.moghe@gmail.com>
2020-04-20 00:40:25 +0530

    Added CountryPicker to the app

--- covid19-tracker/src/components/CountryPicker/CountryPicker.jsx
+++ covid19-tracker/src/components/CountryPicker/CountryPicker.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
-
-const CountryPicker = ()=> {
+import {useState,useEffect} from 'react';
+import styles from './CountryPicker.module.css';
+import {NativeSelect,FormControl} from '@material-ui/core';
+import {fetchCountries} from '../../api';
+const CountryPicker = ({handleCountryChange})=> {
+    const [fetchedCountries,setfetchedCountries] = useState([])
+    useEffect(()=>{
+        const getCountries = async()=>{
+            setfetchedCountries(await fetchCountries())
+        }
+        getCountries();
+    },[setfetchedCountries]);
     return (
-        <h1>CountryPicker</h1>
+        <FormControl className={styles.formControl}>
+            <NativeSelect defaultValue="" onChange={(e)=>handleCountryChange(e.target.value)}>
+                <option value="">Global</option>
+    {fetchedCountries.map((country,i)=><option key={i} value={country}>{country}</option>)}
+            </NativeSelect>
+        </FormControl>
     )
 }
 export default CountryPicker;
